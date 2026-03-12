b082509b5ab44dde9e1affa5d172b739696a437d
inddoo <watashiindonesiana@gmail.com>
2026-03-12 09:03:22 +0700

    buat object dari class

---
+++ lib/tutor-oop/object-class.dart
@@ -0,0 +1,13 @@
+void main(){
+    Mobil mobil1 = Mobil();
+
+    mobil1.merk = "Toyota";
+    mobil1.model = "Corolla";
+    mobil1.tahun = 2020;
+
+    print(mobil1.merk); //Output: Toyota
+    print(mobil1.model); //Output: Corolla
+    print(mobil1.tahun); //Output: 2020
+
+    mobil1.klakson(); //Output: Beep! Beep!
+}
