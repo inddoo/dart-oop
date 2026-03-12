aaec5e2a094603a7caced79a52f66d7a111f1607
inddoo <watashiindonesiana@gmail.com>
2026-03-12 09:10:03 +0700

    parameterized

--- lib/tutor-oop/object-class.dart
+++ lib/tutor-oop/object-class.dart
@@ -4,12 +4,16 @@ class Student {
     int? rollNumber;
 
     //konstruktor
-    Student(this.name, this.age, this.rollNumber);
+    Student({String? name, int?age, int? rollNumber}){
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
 }
 
 void main(){
     //disini student adalah object dari class Student
-    Student student = Student("John", 20, 1);
+    Student student = Student(name: "John", age: 20, rollNumber: 1);
     print("Name: ${student.name}");
     print("Age: ${student.age}");
     print("Roll Number: ${student.rollNumber}");
