0965d44b3d1fc668b0291d451f7ba70f069451ca
inddoo <watashiindonesiana@gmail.com>
2026-03-12 09:04:13 +0700

    penggunaan constructor dalam class mobil

--- lib/tutor-oop/object-class.dart
+++ lib/tutor-oop/object-class.dart
@@ -1,13 +1,23 @@
+class Mobil {
+    String? merk;
+    String? model;
+    int? tahun;
+
+    Mobil(String merk, String model, int tahun){
+        this.merk = merk;
+        this.model = model;
+        this.tahun = tahun;
+    }
+
+    void klakson(){
+        print("Beep! Bepp!");
+    }
+}
+
 void main(){
-    Mobil mobil1 = Mobil();
+    Mobil mobil2 = Mobil("Honda","Civic",2019);
 
-    mobil1.merk = "Toyota";
-    mobil1.model = "Corolla";
-    mobil1.tahun = 2020;
-
-    print(mobil1.merk); //Output: Toyota
-    print(mobil1.model); //Output: Corolla
-    print(mobil1.tahun); //Output: 2020
-
-    mobil1.klakson(); //Output: Beep! Beep!
+    print(mobil2.merk); //Output: Honda
+    print(mobil2.model); //Output: Civic
+    print(mobil2.tahun); //Output: 2019
 }
