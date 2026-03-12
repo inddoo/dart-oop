d2ca5f12fbfd0baa3fe676ee9b42a12ba6436dfc
inddoo <watashiindonesiana@gmail.com>
2026-03-12 09:01:58 +0700

    deklarasi konstructor2

--- lib/tutor-oop/klarasi-kon.dart
+++ lib/tutor-oop/klarasi-kon.dart
@@ -1,20 +1,30 @@
-class Siswa {
+class Guru {
     String? nama;
     int? umur;
-    int? nomorInduk;
+    String? mataPelajaran;
+    double? gaji;
 
-    // konstruktor
-    Siswa(String nama, int umur, int nomorInduk){
-        print("Konstruktor dipanggil");
+    //Konstruktor
+    Guru(String nama, int umur, String mataPelajaran, double gaji){
         this.nama = nama;
         this.umur = umur;
-        this.nomorInduk = nomorInduk;
+        this.mataPelajaran = mataPelajaran;
+        this.gaji = gaji;
+
+    }
+
+    //metode
+    void tampilkan(){
+        print("Nama: ${this.nama}");
+        print("Umur: ${this.umur}");
+        print("Mata Pelajaran: ${this.mataPelajaran}");
+        print("Gaji: ${this.gaji}\n");// \n digunakan untuk baris baru
     }
 }
 
 void main(){
-    Siswa siswa = Siswa("Budi",30,1);
-    print("Nama: ${siswa.nama}");
-    print("Umur: ${siswa.umur}");
-    print("Nomor Induk: ${siswa.nomorInduk}");
+    Guru guru1 = Guru("Budi",30,"Matematika",50000.0);
+    guru1.tampilkan();
+    Guru guru2 = Guru("Cahyo",35,"Sains",60000.0);
+    guru2.tampilkan();
 }
